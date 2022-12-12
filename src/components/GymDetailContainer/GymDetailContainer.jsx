5334e98a3c145d52412de5fed2169f1f45e3d9dd
Nicolás Ríos <riosnicolas618@gmail.com>
2022-12-12 17:25:21 -0300

    Uso de useLocation

--- src/components/GymDetailContainer/GymDetailContainer.jsx
+++ src/components/GymDetailContainer/GymDetailContainer.jsx
@@ -3,11 +3,17 @@ import { useParams } from 'react-router-dom';
 import GymDetail from '../GymDetail/GymDetail';
 import { getDoc, doc } from 'firebase/firestore';
 import dataBase from '../../utils/firebaseConfig';
+import { useLocation } from 'react-router-dom';
 
 const GymDetailContainer = () => {
 
     const [gym, setGym] = useState({})
     const {id} = useParams()
+    const location = useLocation()
+
+    useEffect(() => {
+        window.scrollTo(0,0)
+    }, [location])
 
     useEffect(() => {
         getGym()
